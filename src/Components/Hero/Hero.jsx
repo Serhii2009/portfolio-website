acc8343fc744421f07754f94e45a9971b7ec8760
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-08-05 14:43:47 +0300

    created routes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,10 @@
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
         <span>I’m Serhii Kravchenko,</span> full stack developer based in
@@ -15,7 +16,11 @@ const Hero = () => {
         solutions.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
